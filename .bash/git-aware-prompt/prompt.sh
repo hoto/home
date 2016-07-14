737a1728c0ebd3a16385675c725b7d1bac110c8c
Andrzej Rehmann <andrzej.rehmann@gmail.com>
2016-07-14 14:59:58 +0200

    git commit hash in PS1 (only first 3 letters)

--- .bash/git-aware-prompt/prompt.sh
+++ .bash/git-aware-prompt/prompt.sh
@@ -1,3 +1,7 @@
+find_git_commit_hash() {
+  git_commit_hash=$(git rev-parse HEAD 2> /dev/null)
+}
+
 find_git_branch() {
   # Based on: http://stackoverflow.com/a/13003854/170413
   local branch
@@ -20,7 +24,7 @@ find_git_dirty() {
   fi
 }
 
-PROMPT_COMMAND="find_git_branch; find_git_dirty; $PROMPT_COMMAND"
+PROMPT_COMMAND="find_git_branch; find_git_commit_hash; find_git_dirty; $PROMPT_COMMAND"
 
 # Default Git enabled prompt with dirty state
 # export PS1="\u@\h \w \[$txtcyn\]\$git_branch\[$txtred\]\$git_dirty\[$txtrst\]\$ "
