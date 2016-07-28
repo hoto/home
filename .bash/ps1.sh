39fd42d7e3e5fb34d76154bc148e52f84f0971ac
Andrzej Rehmann <andrzej.rehmann@gmail.com>
2016-07-28 23:33:04 +0200

    removed commit hash from PS1, not using it much anyway anymore

--- .bash/ps1.sh
+++ .bash/ps1.sh
@@ -9,7 +9,6 @@ function show_return_code_if_non_zero() {
 # TODO: Maybe add more colors? https://github.com/itchyny/lightline.vim
 export PS1="\[$baklbl\]\[$txtwht\]\w\
 \[$txtrst\]\[$txtlbl\]\[$bakylw\]\
-\[$txtrst\]\[$bakylw\]\[$txtdgr\] (\${git_commit_hash:0:3})\
 \[$txtrst\]\[$bakylw\]\[$txtblk\]  \$git_branch\
 \[$txtrst\]\[$txtylw\]\
 \[$txtrst\]\[$txtred\]\$git_dirty\
