e62beaa9195996cd9111a112ff6d9e6de613d8b8
Andrzej Rehmann <andrzej.rehmann@gmail.com>
2017-07-13 07:05:39 +0200

    Put new line in PS1

--- .bash/ps1.sh
+++ .bash/ps1.sh
@@ -5,12 +5,12 @@ function show_return_code_if_non_zero() {
   [ $RETVAL -ne 0 ] && echo "[$RETVAL] "
 }
 
-# TODO: Maybe add more colors? https://github.com/itchyny/lightline.vim
 export PS1="\[$bakblu\]\[$txtwht\]\w\
 \[$txtrst\]\[$txtblu\]\[$bakylw\]\
 \[$txtrst\]\[$bakylw\]\[$txtblk\]  \$git_branch\
 \[$txtrst\]\[$txtylw\]\
 \[$txtrst\]\[$txtred\]\$git_dirty\
-\[$txtrst\]\$ \
+\[$txtrst\]\
 \[$txtrst\]\[$txtred\]\$(show_return_code_if_non_zero)\
-\[$txtrst\]"
+\[$txtrst\]\
+\n\$ "
